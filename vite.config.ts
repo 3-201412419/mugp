2fae7b034a6b51ce8fb3a6510cc977d0f6011fa8
3-201412419 <dok2andtablo@naver.com>
2025-01-13 20:22:34 +0900

    01_13 github page 적용 테스트(2)

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mugp/',
   plugins: [react()],
   assetsInclude: ['**/*.PNG', '**/*.png'],
   resolve: {
     alias: {
       '@': '/src',
     },
-  },
+  }
 })
